114c1d1152bff783779f1f61d038f31572ba4a82
inspirionlake <inspirionlake@gmail.com>
2018-07-14 02:02:36 +0300

    Was added setOfFunction.h and setOfFunction.cpp where realised functions, which handle frames from port. Also added handler of incoming frames, which call functions from setOfFunction This version of firmware working with kromSoftware000_archive

---
+++ krom004/setOfFunction.cpp
@@ -0,0 +1,53 @@
+/*
+ * setOfFunction.cpp
+ *
+ * Created: 06.07.2018 15:14:49
+ *  Author: Dmitry
+ */ 
+#include "setOfFunction.h"
+#include "uart.h"
+#include "protocol.h"
+
+class Axis;
+
+void functionEcho(void) {
+	uint8_t frame_echo[5];
+	uint8_t number_of_bytes_echo;
+	uint8_t data_echo[1];
+	uint8_t number_of_data_bytes_echo = 0;
+	makeFrame(frame_echo, &number_of_bytes_echo, 255, data_echo, number_of_data_bytes_echo);
+	sendFrame(frame_echo, number_of_bytes_echo);
+}
+
+void functionOK(void) {
+	uint8_t frame_ok[5];
+	uint8_t number_of_bytes_ok;
+	uint8_t data_ok[1];
+	uint8_t number_of_data_bytes_ok = 0;
+	makeFrame(frame_ok, &number_of_bytes_ok, OK, data_ok, number_of_data_bytes_ok);
+	sendFrame(frame_ok, number_of_bytes_ok);
+}
+
+void functionRepeat(void) {
+	uint8_t frame_repeat[5];
+	uint8_t number_of_bytes_repeat;
+	uint8_t data_repeat[1];
+	uint8_t number_of_data_bytes_repeat = 0;
+	// HERE program must repeat previously frame...
+	makeFrame(frame_repeat, &number_of_bytes_repeat, REPEAT, data_repeat, number_of_data_bytes_repeat);
+	sendFrame(frame_repeat, number_of_bytes_repeat);
+}
+
+void functionGetCoordinateXY(Axis *ax_x, Axis *ax_y) {
+	uint8_t frame_getCoordinateXY[13];
+	uint8_t number_of_bytes_getCoordinateXY;
+	uint8_t data_getCoordinateXY[8];
+	uint8_t number_of_data_bytes_getCoordinateXY = 8;
+		
+	// You should put coordinates of axises into data_getCoordinateXY... 
+	//int32_t coordinate_x = ax_x->getCoordinate();
+	//int32_t coordinate_y = ax_y->getCoordinate();
+	
+	makeFrame(frame_getCoordinateXY, &number_of_bytes_getCoordinateXY, GET_COORDINATE_XY, data_getCoordinateXY, number_of_data_bytes_getCoordinateXY);
+	sendFrame(frame_getCoordinateXY, number_of_bytes_getCoordinateXY);
+}
---
+++ krom004/setOfFunction.h
@@ -0,0 +1,25 @@
+/*
+ * setOfFunction.h
+ *
+ * Created: 06.07.2018 15:14:17
+ *  Author: Dmitry
+ */ 
+
+
+#ifndef SETOFFUNCTION_H_
+#define SETOFFUNCTION_H_
+
+// function's names
+#define ECHO	1
+#define REPEAT	15
+#define OK		16
+#define GET_COORDINATE_XY	2	
+//function's names end
+
+void functionEcho(void);
+void functionOK(void);
+void functionRepeat(void);
+void functionGetCoordinateXY(void);
+
+
+#endif /* SETOFFUNCTION_H_ */
